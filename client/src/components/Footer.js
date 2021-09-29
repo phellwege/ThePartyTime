9156057a810790a04d45f23d5b856739903f788a
Peter <phellwege1@gmail.com>
2021-09-29 12:46:00 -0600

    Party foul 2

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -11,9 +11,12 @@ const Footer = () => {
                     <a href="#Stuff">Stuff</a>
                     <a href="#Stuff">Stuff</a>
                 </div>
-                <div>
+                <div id="footer_links">
                     <a href="https://github.com/">
                         Git
+                    </a>
+                    <a href="https://">
+                        Another link
                     </a>
                 </div>
         </footer>
