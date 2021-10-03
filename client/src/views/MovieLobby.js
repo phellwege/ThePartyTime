f4936077d6e5fa16735af3148faa50db3419dbc9
Peter <phellwege1@gmail.com>
2021-10-03 13:40:37 -0600

    added crashed ship

--- client/src/views/MovieLobby.js
+++ client/src/views/MovieLobby.js
@@ -6,57 +6,58 @@ import Kiosk from '../static/Kiosk.gif';
 const MovieTheatre = () => {
     return (
         <div id="Movie_Wrap">
-        <Head/>
-        <div id="MovieBoard">
-            <h3>Currently Playing</h3>
-            <table>
-                    <tr>
-                        <th>Halloween</th>
-                        <th>Childrens</th>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="/Movie1">
-                                Nightmare Before Christmas
-                            </a> 
-                        </td>
-                        <td>
-                            <a href="/Movie4">
-                                Zootopia
-                            </a> 
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="/Movie3">
-                                Halloween
-                            </a> 
-                        </td>
-                        <td>
-                            <a href="/Movie5">
-                                Pocahontas
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><a href="/Movie2">
-                                Coraline
-                            </a> </td>
-                        
-                        <td>
-                            <a href="/Movie6">
-                                Rise of the Guardians
-                            </a>
-                        </td>
-                    </tr>
-            </table>
-        </div>
-        <div id="octo_pop">
-            <img src={Octo_pop} alt="octo_pop"/>
-        </div>
-        <div id="robo_kiosk">
-            <img src={Kiosk} alt="robo_kiosk" id="robo_kiosk"/>
-        </div>
+            <Head/>
+            <div id="MovieBoard">
+                <h3>Currently Playing</h3>
+                <table>
+                        <tr>
+                            <th>Halloween</th>
+                            <th>Childrens</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a href="/Movie1">
+                                    Nightmare Before Christmas
+                                </a> 
+                            </td>
+                            <td>
+                                <a href="/Movie4">
+                                    Zootopia
+                                </a> 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a href="/Movie3">
+                                    Halloween
+                                </a> 
+                            </td>
+                            <td>
+                                <a href="/Movie5">
+                                    Pocahontas
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><a href="/Movie2">
+                                    Coraline
+                                </a> </td>
+                            
+                            <td>
+                                <a href="/Movie6">
+                                    Rise of the Guardians
+                                </a>
+                            </td>
+                        </tr>
+                </table>
+            </div>
+            
+            <div id="robo_kiosk">
+                <img src={Kiosk} alt="robo_kiosk" id="robo_kiosk"/>
+            </div>
+            <div id="octo_pop">
+                <img src={Octo_pop} alt="octo_pop"/>
+            </div>
         </div>
     )
 }
