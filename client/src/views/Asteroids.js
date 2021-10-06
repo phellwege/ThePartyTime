7fc0ae7436154036d4852dada30d3eec8e25e460
Peter <phellwege1@gmail.com>
2021-10-06 10:36:38 -0600

    added asteroids background

--- client/src/views/Asteroids.js
+++ client/src/views/Asteroids.js
@@ -1,9 +1,21 @@
 import "../Events.css";
 import {Link} from '@reach/router';
-
+import Head from '../components/Head';
+import Footer from '../components/Footer';
+import Cockpit from '../static/Cockpit.gif'
 const Asteroids = () => {
     return (
         <>
+        <Head/>
+        <div id="Cockpit_View">
+            <div id="Asteroids_Game_Window">
+
+            </div>
+            <div id="Alien_Driver">
+                <img src={Cockpit} alt="Alien Driving SpaceShip" id="Driving"/>
+            </div>
+        </div>
+        <Footer/>
         </>
     )
 }
