956591821e42c8e0102f96cf9ee003656105586d
Peter <phellwege1@gmail.com>
2021-10-05 21:15:58 -0600

    Started Paint Layout

--- client/src/views/Paint.js
+++ client/src/views/Paint.js
@@ -1,9 +1,29 @@
 import "../Events.css";
 import {Link} from '@reach/router';
+import Head from '../components/Head';
+import '../Main.css';
+import Footer from "../components/Footer";
 
 const Paint = () => {
     return (
         <>
+        <Head/>
+            <div>
+                <h1>Art</h1>
+            </div>
+            <div id="Artist">
+                <div id="Art_Canvas">
+
+                </div>
+                <div id="Art_Tools">
+                    <ul>
+                        <li>this</li>
+                        <li>that</li>
+                        <li>the other</li>
+                    </ul>
+                </div>
+            </div>
+            <Footer/>
         </>
     )
 }
