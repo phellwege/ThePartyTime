13114ddc28df0a23c3eb38822f94eb4705deb953
Peter <phellwege1@gmail.com>
2021-10-06 11:50:35 -0600

    paint canvas started

--- client/src/views/Paint.js
+++ client/src/views/Paint.js
@@ -4,6 +4,22 @@ import Head from '../components/Head';
 import '../Main.css';
 import Footer from "../components/Footer";
 
+function componentDidMount() {
+
+        var canvas = document.getElementById('Art_Canvas');
+        var ctx = canvas.getContext('2d');
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+            ctx.fillStyle = 'rgb(200, 0, 0)';
+                ctx.fillRect(10, 10, 50, 50);
+                ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+                ctx.fillRect(30, 30, 50, 50);
+            // drawing code here
+        } else {
+            // canvas-unsupported code here
+            <p> Your Browser isn't supported for Canvas</p>
+        }
+}
 const Paint = () => {
     return (
         <div id="Paint_Wrapper">
@@ -12,9 +28,9 @@ const Paint = () => {
                 <h1>Art</h1>
             </div>
             <div id="Artist">
-                <div id="Art_Canvas">
+                <canvas id="Art_Canvas" alt="Artists' Canvas">
 
-                </div>
+                </canvas>
                 <div id="Art_Tools">
                     <ul>
                         <li>this</li>
