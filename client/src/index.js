c7d36ba9c772ba58358de5a8ba57ac33ce600467
Peter <phellwege1@gmail.com>
2021-10-17 09:57:10 -0600

    gh pages 404 doc added

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+(function(l) {
+  if (l.search[1] === '/' ) {
+    var decoded = l.search.slice(1).split('&').map(function(s) { 
+      return s.replace(/~and~/g, '&')
+    }).join('?');
+    window.history.replaceState(null, null,
+        l.pathname.slice(0, -1) + decoded + l.hash
+    );
+  }
+}(window.location));
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
